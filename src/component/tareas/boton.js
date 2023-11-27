fde6cfef41a6608dd425e578a83fad65261a4e63
Javierno9911 <47241759+Javierno9911@users.noreply.github.com>
2023-11-27 13:42:52 -0200

    v 1.0.2
    Borrar pruebas

--- src/component/tareas/boton.js
+++
@@ -1,9 +0,0 @@
-import '../recursos/listado.css';
-
-function Boton(props) {
-    return (
-        <button className='boton' onClick={props.onClick}>Añadir Tarea</button>
-    );
-}
-
-export default Boton;
